7eb7a47337706bd636a846e5167260cf839c4159
Glenn Patrick Williams <33417082+gpwilliams@users.noreply.github.com>
2021-05-06 15:47:50 +0100

    Added data summaries for study 3

---
+++ R/03_study-03/99c_make-data-summaries.R
@@ -0,0 +1,23 @@
+# clean raw data ----
+
+library(tidyverse) # for data processing/plotting
+library(lubridate) # for dates
+library(here) # for file paths
+library(janitor) # for cleaning names
+library(patchwork) # for combining plots
+
+# load functions
+r_function_list <- list.files(
+  path = here::here("R", "00_functions"), 
+  pattern = "R$",
+  full.names = TRUE
+)
+purrr::walk(r_function_list, source)
+
+r_file_list <- list.files(
+  path = here::here("R", "02_study-03", "03_make-data-summaries"), 
+  pattern = "R$",
+  full.names = TRUE
+)
+# purrr::walk(r_file_list[1:10], source) # for testing
+purrr::walk(r_file_list, source)
